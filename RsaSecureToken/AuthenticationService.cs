04cc52a77547208580ff2e52e40a1b1b14f0f43a
aprilwu1008 <april.wu@titansoft.com.sg>
2019-07-05 18:43:26 +0800

    AuthenticationServiceTests finished.

--- RsaSecureToken/AuthenticationService.cs
+++ RsaSecureToken/AuthenticationService.cs
@@ -3,20 +3,46 @@ using System.Runtime.Remoting.Messaging;
 
 namespace RsaSecureToken
 {
+    public interface IProfileDao
+    {
+        int GetRegisterTimeInMinutes(string account);
+    }
+
+    public interface IRsaTokenDao
+    {
+        Random GetRandom(int minutes);
+    }
+
     public class AuthenticationService
     {
-        public bool IsValid(string account, string password)
+        private IProfileDao _profileDao;
+        private IRsaTokenDao _rsaTokenDao;
+
+        public AuthenticationService()
+        {
+            _profileDao = new ProfileDao();
+            _rsaTokenDao = new RsaTokenDao();
+        }
+
+        public AuthenticationService(IProfileDao profileDao, IRsaTokenDao rsaTokenDao)
+        {
+            _profileDao = profileDao;
+            _rsaTokenDao = rsaTokenDao;
+        }
+
+        public virtual bool IsValid(string account, string password)
         {
             // 根據 account 取得設定時間
-            var profileDao = new ProfileDao();
+            var profileDao = _profileDao;
             var registerMinutes = profileDao.GetRegisterTimeInMinutes(account);
 
             // 根據 registerMinutes 取得 RSA token 目前的亂數
-            var rsaToken = new RsaTokenDao();
+            var rsaToken = _rsaTokenDao;
             var seed = rsaToken.GetRandom(registerMinutes);
 
             // 驗證傳入的 password 是否等於 otp
-            var isValid = password == seed.Next(0, 999999).ToString("000000"); ;
+            var otp = seed.Next(0, 999999).ToString("000000");
+            var isValid = password == otp;
 
             if (isValid)
             {
@@ -29,18 +55,18 @@ namespace RsaSecureToken
         }
     }
 
-    public class ProfileDao
+    public class ProfileDao : IProfileDao
     {
-        public int GetRegisterTimeInMinutes(string account)
+        public virtual int GetRegisterTimeInMinutes(string account)
         {
             //Not complete yet.
             throw new NotImplementedException();
         }
     }
 
-    public class RsaTokenDao
+    public class RsaTokenDao : IRsaTokenDao
     {
-        public Random GetRandom(int minutes)
+        public virtual Random GetRandom(int minutes)
         {
             return new Random((int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMinutes - minutes);
         }
--- RsaSecureTokenTests/AuthenticationServiceTests.cs
+++ RsaSecureTokenTests/AuthenticationServiceTests.cs
@@ -14,12 +14,30 @@ namespace RsaSecureToken.Tests
         [TestMethod()]
         public void IsValidTest()
         {
-            var sut = new AuthenticationService();
+            var fakeProfileDao = new FakeProfileDao();
+            var fakeRsaTokenDao = new FakeRsaTokenDao();
+            var sut = new AuthenticationService(fakeProfileDao, fakeRsaTokenDao);
 
             // implement your own act
-            var actual = sut.IsValid("April", "");
+            var actual = sut.IsValid("April", "330959");
 
             Assert.IsTrue(actual);
+        }
+    }
+
+    public class FakeProfileDao : IProfileDao
+    {
+        public int GetRegisterTimeInMinutes(string account)
+        {
+            return 1008;
+        }
+    }
+
+    public class FakeRsaTokenDao : IRsaTokenDao
+    {
+        public Random GetRandom(int minutes)
+        {
+            return new Random(1008);
         }
     }
 }
